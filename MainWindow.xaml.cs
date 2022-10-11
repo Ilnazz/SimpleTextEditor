592318db3ac60b9402db771f946a4d7d3d1e3116
Anudiss <liobara31@gmail.com>
2022-10-11 22:07:11 +0300

    Создание нового окна, Отмена действия, Вырезать, копировать, вставить, удалить, zoom

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,6 +32,22 @@ namespace TextEditor
         private string Filter => string.Join("|", FileFilter.Select(keyPair => $"{keyPair.Key} ({string.Join(", ", keyPair.Value)})|{string.Join(";", keyPair.Value)}"));
         private string DefaultFilter => FileFilter.Select(keyPair => $"{keyPair.Key} ({string.Join(", ", keyPair.Value)})|{string.Join(";", keyPair.Value)}").First();
 
+        private MenuItem[] menuItems;
+
+        public const int DefaultScale = 14;
+        private int _scale = DefaultScale;
+
+        public int Scale
+        {
+            get => _scale;
+            set
+            {
+                _scale = value;
+                TB.FontSize = _scale;
+            }
+        }
+
+
         private readonly Microsoft.Office.Interop.Word.Application _application;
         private Document _activeDocument;
 
@@ -51,12 +67,47 @@ namespace TextEditor
         public MainWindow()
         {
             InitializeComponent();
+
+            menuItems = new MenuItem[TopMenu.Items.Count];
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                if (TopMenu.Items[i] is MenuItem menuItem)
+                    if (menuItem.IsEnabled == false)
+                        menuItems[i] = menuItem;
+            
+            }
+
             _application = new Microsoft.Office.Interop.Word.Application();
+            CreateNewDocument();
         }
 
         private void CreateNewDocument()
         {
+            if (ActiveDocument == null)
+            {
+                AddDocument();
+                TB.Text = string.Empty;
+            }
+            else
+            {
+                SaveDocument();
+                if (_isSaved == false)
+                    return;
 
+                CloseDocument();
+
+                AddDocument();
+                ReadDocument();
+            }
+        }
+
+        private void AddDocument()
+        {
+            object template = Type.Missing;
+            object newTemplate = false;
+            object documentType = WdNewDocumentType.wdNewBlankDocument;
+            object visible = true;
+            ActiveDocument = _application.Documents.Add(ref template, ref newTemplate, ref documentType, ref visible);
         }
 
         private void OpenDocument()
@@ -74,19 +125,41 @@ namespace TextEditor
 
         private void SaveDocument()
         {
+            if (_isSaved)
+                return;
+
+            if (ActiveDocument == null)
+                return;
+
+
             if (_path == null)
+            {
                 SaveAsDocument();
+            }
             else
+            {
+                ActiveDocument.Content.Text = TB.Text;
                 ActiveDocument.Save();
+                _isSaved = true;
+            }
         }
 
         private void SaveAsDocument()
         {
+            if (_isSaved)
+                return;
+
+            if (ActiveDocument == null)
+                return;
+
             _path = CreateSaveFileDialogMenu();
             if (_path == null)
                 return;
 
-            ActiveDocument.SaveAs2(_path);
+            ActiveDocument.Content.Text = TB.Text;
+            object pathObject = _path;
+            ActiveDocument.SaveAs2(ref pathObject);
+            _isSaved = true;
         }
 
         private string CreateSaveFileDialogMenu()
@@ -128,6 +201,12 @@ namespace TextEditor
             if (ActiveDocument == null)
                 return;
 
+            MessageBoxResult wantSave = MessageBox.Show("Хотите сохранить?", "Сохранить изменения?", MessageBoxButton.YesNoCancel);
+            if (wantSave == MessageBoxResult.Cancel)
+                return;
+            else if (wantSave == MessageBoxResult.Yes)
+                SaveDocument();
+
             ActiveDocument.Close();
             ActiveDocument = null;
         }
@@ -148,7 +227,103 @@ namespace TextEditor
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            QuitApplication();
+            CloseDocument();
+            if (ActiveDocument == null)
+                _application.Quit();
+            else
+                e.Cancel = true;
+        }
+
+        private void TB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            _isSaved = false;
+        }
+
+        private void CreateNewDocument_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewDocument();
+        }
+
+        private void NewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+        }
+
+        private void OpenDocument_Click(object sender, RoutedEventArgs e)
+        {
+            OpenDocument();
+        }
+
+        private void SaveDocument_Click(object sender, RoutedEventArgs e)
+        {
+            SaveDocument();
+        }
+
+        private void SaveAsDocument_Click(object sender, RoutedEventArgs e)
+        {
+            SaveAsDocument();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Undo_Click(object sender, RoutedEventArgs e)
+        {
+            TB.Undo();
+        }
+
+        private void Cut_Click(object sender, RoutedEventArgs e)
+        {
+            TB.Cut();
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            TB.Copy();
+        }
+
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            TB.Paste();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            string text = TB.Text;
+            TB.Text = text.Remove(TB.SelectionStart, TB.SelectionLength);
+        }
+
+        private void TB_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            bool enabled = TB.SelectionLength > 0;
+            if (enabled == false)
+            {
+                foreach (var item in menuItems)
+                    if (item != null)
+                        item.IsEnabled = false;
+                return;
+            }
+            foreach (var menuItem in menuItems)
+                if (menuItem != null)
+                    menuItem.IsEnabled = enabled;
+        }
+
+        private void ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            Scale = (int)(Scale * 1.3);
+        }
+
+        private void ZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+            Scale = (int)(Scale / 1.3);
+        }
+
+        private void DefaultZomm_Click(object sender, RoutedEventArgs e)
+        {
+            Scale = DefaultScale;
         }
     }
 }
